aa2b6cfa901b328c0d06de8a895bdf2e43dbe6bd
last_one <last_onephy@gmail.com>
2017-11-02 10:49:08 +0800

    update

---
+++ testing/test.sh
@@ -0,0 +1,2 @@
+LOG="log/train-`date +'%Y-%m-%d-%H-%M-%S'`.log"
+nohup python train_pose.py --gpu 0 1 --train_dir /home/hypan/data/coco/filelist/train2014.txt /home/hypan/data/coco/masklist/train2014.txt /home/hypan/data/coco/json/train2014.json --val_dir /home/hypan/data/coco/filelist/train2014.txt /home/hypan/data/coco/masklist/train2014.txt /home/hypan/data/coco/json/train2014.json --config config.yml
