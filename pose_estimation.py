5615912980dbf885b5dee61b2322f5b54857f234
last_one <last_onephy@gmail.com>
2017-11-04 23:51:20 +0800

    change the std of initlization

--- pose_estimation.py
+++ pose_estimation.py
@@ -34,7 +34,7 @@ class Pose_Estimation(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.weight.data.normal_(0, 0.01)
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
