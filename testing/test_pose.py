72dfeb9cfc3b6d3dabfc9539f853f83930127f53
last_one <last_onephy@gmail.com>
2017-11-11 23:36:45 +0800

    update test script

--- testing/test_pose.py
+++ testing/test_pose.py
@@ -10,31 +10,23 @@ import torch
 import pose_estimation
 import cv2
 
-# find connection in the specified sequence, center 29 is in the position 15
-# limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10], \
-           # [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17], \
-           # [1, 16], [16, 18], [3, 17], [6, 18]]
-limbSeq = [[3,4],[4,5],[6,7],[7,8],[9,10],[10,11],[12,13],[13,14],[1,2],[2,9],[2,12], [2,3],[2,6],[3,17],[6,18],[1,16],[1,15],[16,18],[15,17]]
+limbSeq = [[3,4], [4,5], [6,7], [7,8], [9,10], [10,11], [12,13], [13,14], [1,2], [2,9], [2,12], [2,3], [2,6], \
+           [3,17],[6,18],[1,16],[1,15],[16,18],[15,17]]
 
-# the middle joints heatmap correpondence
-# mapIdx = [[31, 32], [39, 40], [33, 34], [35, 36], [41, 42], [43, 44], [19, 20], [21, 22], \
-          # [23, 24], [25, 26], [27, 28], [29, 30], [47, 48], [49, 50], [53, 54], [51, 52], \
-          # [55, 56], [37, 38], [45, 46]]
-mapIdx = [[19,20],[21,22],[23,24],[25,26],[27,28],[29,30],[31,32],[33,34],[35,36],[37,38],[39,40],[41,42],[43,44],[45,46],[47,48],[49,50],[51,52],[53,54],[55,56]]
+mapIdx = [[19,20],[21,22],[23,24],[25,26],[27,28],[29,30],[31,32],[33,34],[35,36],[37,38],[39,40], \
+          [41,42],[43,44],[45,46],[47,48],[49,50],[51,52],[53,54],[55,56]]
 
-# visualize
-colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0],
-          [0, 255, 0], \
-          [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255],
-          [85, 0, 255], \
+colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
+          [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
           [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
 
-scale_search = [0.5, 1., 1.5, 2.]
 boxsize = 368
+scale_search = [0.5, 1.0, 1.5, 2.0]
 stride = 8
-pad_value = 0
-thre1 = 0.1
-thre2 = 0.05
+padValue = 0.
+thre_point = 0.15
+thre_line = 0.05
+stickwidth = 4
 
 def construct_model(args):
 
@@ -48,12 +40,12 @@ def construct_model(args):
     state_dict = model.state_dict()
     state_dict.update(new_state_dict)
     model.load_state_dict(state_dict)
-    # model.fc = nn.Linear(2048, 80)
     model = model.cuda()
 
     return model
 
 def padRightDownCorner(img, stride, padValue):
+
     h = img.shape[0]
     w = img.shape[1]
 
@@ -75,57 +67,60 @@ def padRightDownCorner(img, stride, padValue):
 
     return img_padded, pad
 
-def process (model, input_image):
+def normalize(origin_img):
 
-    origin_image = cv2.imread(input_image)  # B,G,R order
-    origin_image = np.array(origin_image, dtype=np.float32)
-    origin_image -= 128.0
-    origin_image /= 256.0
-    print origin_image.shape
-    h, w, _ = origin_image.shape
-    multiplier = [x * boxsize / h for x in scale_search]
 
-    heatmap_avg = np.zeros((origin_image.shape[0], origin_image.shape[1], 19))
-    paf_avg = np.zeros((origin_image.shape[0], origin_image.shape[1], 38))
+    origin_img = np.array(origin_img, dtype=np.float32)
+    origin_img -= 128.0
+    origin_img /= 256.0
+
+    return origin_img
+
+def process(model, input_path):
+
+    origin_img = cv2.imread(input_path)
+    normed_img = normalize(origin_img)
+
+    height, width, _ = normed_img.shape
+
+    multiplier = [x * boxsize / height for x in scale_search]
+
+    heatmap_avg = np.zeros((height, width, 19)) # num_point
+    paf_avg = np.zeros((height, width, 38))     # num_vector
 
     for m in range(len(multiplier)):
         scale = multiplier[m]
 
-        imageToTest = cv2.resize(origin_image, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-        imageToTest_padded, pad = padRightDownCorner(imageToTest, stride, pad_value)
+        # preprocess
+        imgToTest = cv2.resize(normed_img, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+        imgToTest_padded, pad = padRightDownCorner(imgToTest, stride, padValue)
 
-        input_img = np.transpose(np.float32(imageToTest_padded[:,:,:,np.newaxis]), (3,2,0,1)) # required shape (1, width, height, channels)
-        mask = np.ones((1, 1, input_img.shape[2]/stride, input_img.shape[3]/stride), dtype=np.float32)
+        input_img = np.transpose(imgToTest_padded[:,:,:,np.newaxis], (3, 2, 0, 1)) # required shape (1, c, h, w)
+        mask = np.ones((1, 1, input_img.shape[2] / stride, input_img.shape[3] / stride), dtype=np.float32)
+
         input_var = torch.autograd.Variable(torch.from_numpy(input_img).cuda())
         mask_var = torch.autograd.Variable(torch.from_numpy(mask).cuda())
 
+        # get the features
         vec1, heat1, vec2, heat2, vec3, heat3, vec4, heat4, vec5, heat5, vec6, heat6 = model(input_var, mask_var)
-        heat6 = heat6.data.cpu().numpy()
-        vec6 = vec6.data.cpu().numpy()
 
-        # extract outputs, resize, and remove padding
-        heatmap = np.transpose(np.squeeze(heat6), [1, 2, 0])  # output 1 is heatmaps
+        # get the heatmap
+        heatmap = heat6.data.cpu().numpy()
+        heatmap = np.transpose(np.squeeze(heatmap), (1, 2, 0)) # (h, w, c)
         heatmap = cv2.resize(heatmap, (0, 0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
-        heatmap = heatmap[:imageToTest_padded.shape[0] - pad[2], :imageToTest_padded.shape[1] - pad[3],:]
-        heatmap = cv2.resize(heatmap, (origin_image.shape[1], origin_image.shape[0]), interpolation=cv2.INTER_CUBIC)
-
-        paf = np.transpose(np.squeeze(vec6), [1, 2, 0])  # output 0 is PAFs
-        paf = cv2.resize(paf, (0, 0), fx=stride, fy=stride,interpolation=cv2.INTER_CUBIC)
-        paf = paf[:imageToTest_padded.shape[0] - pad[2], :imageToTest_padded.shape[1] - pad[3], :]
-        paf = cv2.resize(paf, (origin_image.shape[1], origin_image.shape[0]), interpolation=cv2.INTER_CUBIC)
-        # for xx in range(1,19):
-            # cv2.imwrite('heatmap_{}_{}.jpg'.format(scale, xx), heatmap[...,xx]*255)
-        # for xx in range(1,19):
-            # cv2.imwrite('vec_{}_{}.jpg'.format(scale, xx), (paf[...,xx*2] + paf[...,xx*2+1])*255)
-
+        heatmap = heatmap[:imgToTest_padded.shape[0] - pad[2], :imgToTest_padded.shape[1] - pad[3], :]
+        heatmap = cv2.resize(heatmap, (width, height), interpolation=cv2.INTER_CUBIC)
         heatmap_avg = heatmap_avg + heatmap / len(multiplier)
+
+        # get the paf
+        paf = vec6.data.cpu().numpy()
+        paf = np.transpose(np.squeeze(paf), (1, 2, 0)) # (h, w, c)
+        paf = cv2.resize(paf, (0, 0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
+        paf = paf[:imgToTest_padded.shape[0] - pad[2], :imgToTest_padded.shape[1] - pad[3], :]
+        paf = cv2.resize(paf, (width, height), interpolation=cv2.INTER_CUBIC)
         paf_avg = paf_avg + paf / len(multiplier)
 
-    # for xx in range(1,19):
-        # cv2.imwrite('avg_heatmap_{}_{}.jpg'.format(scale, xx), heatmap_avg[...,xx]*127.5)
-    # for xx in range(1,19):
-        # cv2.imwrite('avg_vec_{}_{}.jpg'.format(scale, xx), (paf_avg[...,xx*2] + paf_avg[...,xx*2+1])*127.5)
-    all_peaks = []
+    all_peaks = []   # all of the possible points by classes.
     peak_counter = 0
 
     for part in range(1, 19):
@@ -133,82 +128,79 @@ def process (model, input_image):
         map = gaussian_filter(map_ori, sigma=3)
 
         map_left = np.zeros(map.shape)
-        map_left[1:, :] = map[:-1, :]
+        map_left[:, 1:] = map[:, :-1]
         map_right = np.zeros(map.shape)
-        map_right[:-1, :] = map[1:, :]
+        map_right[:, :-1] = map[:, 1:]
         map_up = np.zeros(map.shape)
-        map_up[:, 1:] = map[:, :-1]
+        map_up[1:, :] = map[:-1, :]
         map_down = np.zeros(map.shape)
-        map_down[:, :-1] = map[:, 1:]
+        map_down[:-1, :] = map[1:, :]
 
+        # get the salient point and its score > thre_point
         peaks_binary = np.logical_and.reduce(
-            (map >= map_left, map >= map_right, map >= map_up, map >= map_down, map > thre1))
-        peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0]))  # note reverse
+                (map >= map_left, map >= map_right, map >= map_up, map >= map_down, map > thre_point))
+        peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0])) # (w, h)
+        
+        # a point format: (w, h, score, number)
         peaks_with_score = [x + (map_ori[x[1], x[0]],) for x in peaks]
         id = range(peak_counter, peak_counter + len(peaks))
-        peaks_with_score_and_id = [peaks_with_score[i] + (id[i],) for i in range(len(id))]
+        peaks_with_score_and_id = [peaks_with_score[i] + (id[i], ) for i in range(len(id))]
 
         all_peaks.append(peaks_with_score_and_id)
         peak_counter += len(peaks)
 
-    connection_all = []
-    special_k = []
-    mid_num = 10
+    connection_all = [] # save all of the possible lines by classes.
+    special_k = []      # save the lines, which haven't legal points.
+    mid_num = 10        # could adjust to accelerate (small) or improve accuracy(large).
 
     for k in range(len(mapIdx)):
+
         score_mid = paf_avg[:, :, [x - 19 for x in mapIdx[k]]]
         candA = all_peaks[limbSeq[k][0] - 1]
         candB = all_peaks[limbSeq[k][1] - 1]
-        nA = len(candA)
-        nB = len(candB)
-        indexA, indexB = limbSeq[k]
-        if (nA != 0 and nB != 0):
+
+        lenA = len(candA)
+        lenB = len(candB)
+
+        if lenA != 0 and lenB != 0:
             connection_candidate = []
-            for i in range(nA):
-                for j in range(nB):
-                    vec = np.subtract(candB[j][:2], candA[i][:2])
+            for i in range(lenA):
+                for j in range(lenB):
+                    vec = np.subtract(candB[j][:2], candA[i][:2]) # the vector of BA
                     norm = math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
-                    # failure case when 2 body parts overlaps
                     if norm == 0:
                         continue
                     vec = np.divide(vec, norm)
 
-                    startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
-                                   np.linspace(candA[i][1], candB[j][1], num=mid_num)))
+                    startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), np.linspace(candA[i][1], candB[j][1], num=mid_num)))
 
-                    vec_x = np.array(
-                        [score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] \
-                         for I in range(len(startend))])
-                    vec_y = np.array(
-                        [score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] \
-                         for I in range(len(startend))])
+                    # get the vector between A and B.
+                    vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] for I in range(len(startend))])
+                    vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] for I in range(len(startend))])
 
                     score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
-                    score_with_dist_prior = sum(score_midpts) / len(score_midpts) + min(
-                        0.5 * origin_image.shape[0] / norm - 1, 0)
-                    criterion1 = len(np.nonzero(score_midpts > thre2)[0]) > 0.8 * len(
-                        score_midpts)
+                    score_with_dist_prior = sum(score_midpts) / len(score_midpts) + min(0.5 * height / norm - 1, 0) # ???
+                    criterion1 = len(np.nonzero(score_midpts > thre_line)[0]) > 0.8 * len(score_midpts)
                     criterion2 = score_with_dist_prior > 0
                     if criterion1 and criterion2:
-                        connection_candidate.append([i, j, score_with_dist_prior,
-                                                     score_with_dist_prior + candA[i][2] + candB[j][2]])
+                        connection_candidate.append([i, j, score_with_dist_prior, score_with_dist_prior + candA[i][2], candB[j][2]])
 
+            # sort the possible line from large to small order.
             connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
             connection = np.zeros((0, 5))
-            for c in range(len(connection_candidate)):
-                i, j, s = connection_candidate[c][0:3]
-                if (i not in connection[:, 3] and j not in connection[:, 4]):
-                    connection = np.vstack([connection, [candA[i][3], candB[j][3], s, i, j]])
-                    if (len(connection) >= min(nA, nB)):
-                        break
 
+            for c in range(len(connection_candidate)):
+                i, j, s = connection_candidate[c][0: 3]
+                if (i not in connection[:, 3] and j not in connection[:, 4]):
+                    # the number of A point, the number of B point, score, A point, B point
+                    connection = np.vstack([connection, [candA[i][3], candB[j][3], s, i, j]]) 
+                    if len(connection) >= min(lenA, lenB):
+                        break
             connection_all.append(connection)
         else:
             special_k.append(k)
             connection_all.append([])
 
-    # last number in each row is the total parts number of that person
-    # the second last number in each row is the score of the overall configuration
     subset = -1 * np.ones((0, 20))
     candidate = np.array([item for sublist in all_peaks for item in sublist])
 
@@ -218,13 +210,33 @@ def process (model, input_image):
             partBs = connection_all[k][:, 1]
             indexA, indexB = np.array(limbSeq[k]) - 1
 
-            for i in range(len(connection_all[k])):  # = 1:size(temp,1)
+            for i in range(len(connection_all[k])):
                 found = 0
+                flag = [False, False]
                 subset_idx = [-1, -1]
-                for j in range(len(subset)):  # 1:size(subset,1):
-                    if subset[j][indexA] == partAs[i] or subset[j][indexB] == partBs[i]:
-                        subset_idx[found] = j
-                        found += 1
+                for j in range(len(subset)):
+                    # fix the bug, found == 2 and not joint will lead someone occur more than once.
+                    # if more than one, we choose the subset, which has a higher score.
+                    if subset[j][indexA] == partAs[i]:
+                        if flag[0] == False:
+                            flag[0] = found
+                            subset_idx[found] = j
+                            flag[0] = True
+                            found += 1
+                        else:
+                            ids = subset_idx[flag[0]]
+                            if subset[ids][-1] < subset[j][-1]:
+                                subset_idx[flag[0]] = j
+                    if subset[j][indexB] == partBs[i]:
+                        if flag[1] == False:
+                            flag[1] = found
+                            subset_idx[found] = j
+                            flag[1] = True
+                            found += 1
+                        else:
+                            ids = subset_idx[flag[1]]
+                            if subset[ids][-1] < subset[j][-1]:
+                                subset_idx[flag[1]] = j
 
                 if found == 1:
                     j = subset_idx[0]
@@ -232,46 +244,41 @@ def process (model, input_image):
                         subset[j][indexB] = partBs[i]
                         subset[j][-1] += 1
                         subset[j][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
-                elif found == 2:  # if found 2 and disjoint, merge them
+                elif found == 2: # if found equals to 2 and disjoint, merge them
                     j1, j2 = subset_idx
                     membership = ((subset[j1] >= 0).astype(int) + (subset[j2] >= 0).astype(int))[:-2]
-                    if len(np.nonzero(membership == 2)[0]) == 0:  # merge
+                    if len(np.nonzero(membership == 2)[0]) == 0: # merge
                         subset[j1][:-2] += (subset[j2][:-2] + 1)
                         subset[j1][-2:] += subset[j2][-2:]
                         subset[j1][-2] += connection_all[k][i][2]
                         subset = np.delete(subset, j2, 0)
-                    else:  # as like found == 1
+                    else: # as like found == 1
                         subset[j1][indexB] = partBs[i]
                         subset[j1][-1] += 1
                         subset[j1][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
-
-                # if find no partA in the subset, create a new subset
-                elif not found:
+                elif not found and k < 17:
                     row = -1 * np.ones(20)
                     row[indexA] = partAs[i]
                     row[indexB] = partBs[i]
                     row[-1] = 2
-                    row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + \
-                              connection_all[k][i][2]
+                    row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + connection_all[k][i][2]
                     subset = np.vstack([subset, row])
 
-    # delete some rows of subset which has few parts occur
-    deleteIdx = [];
+    # delete som rows of subset which has few parts occur
+    deleteIdx = []
     for i in range(len(subset)):
         if subset[i][-1] < 4 or subset[i][-2] / subset[i][-1] < 0.4:
             deleteIdx.append(i)
     subset = np.delete(subset, deleteIdx, axis=0)
 
-    canvas = cv2.imread(input_image)  # B,G,R order
+    # draw points
+    canvas = cv2.imread(input_path)
     for i in range(18):
         for j in range(len(all_peaks[i])):
-            if all_peaks[i][j][2] < 0.25:
-                continue
             cv2.circle(canvas, all_peaks[i][j][0:2], 4, colors[i], thickness=-1)
 
-    stickwidth = 2
-
-    for i in range(19):
+    # draw lines
+    for i in range(17):
         for n in range(len(subset)):
             index = subset[n][np.array(limbSeq[i]) - 1]
             if -1 in index:
@@ -283,15 +290,15 @@ def process (model, input_image):
             mY = np.mean(Y)
             length = ((X[0] - X[1]) ** 2 + (Y[0] - Y[1]) ** 2) ** 0.5
             angle = math.degrees(math.atan2(X[0] - X[1], Y[0] - Y[1]))
-            polygon = cv2.ellipse2Poly((int(mY), int(mX)), (int(length / 2), stickwidth), int(angle), 0,
-                                       360, 1)
+            polygon = cv2.ellipse2Poly((int(mY), int(mX)), (int(length / 2), stickwidth), int(angle), 0, 360, 1)
             cv2.fillConvexPoly(cur_canvas, polygon, colors[i])
             canvas = cv2.addWeighted(canvas, 0.4, cur_canvas, 0.6, 0)
 
     return canvas
 
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '3'
     parser = argparse.ArgumentParser()
     parser.add_argument('--image', type=str, required=True, help='input image')
     parser.add_argument('--output', type=str, default='result.png', help='output image')
@@ -301,14 +308,13 @@ if __name__ == '__main__':
     input_image = args.image
     output = args.output
 
+
+    # load model
     model = construct_model(args)
 
     tic = time.time()
     print('start processing...')
 
-    # load model
-
-    # load config
     # generate image with body parts
     canvas = process(model, input_image)
 
